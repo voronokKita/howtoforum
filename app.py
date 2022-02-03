03062b634528886046a064c4f63f06600983bced
Kita <kitavoronok@gmail.com>
2022-02-03 14:14:28 +0300

    login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, render_template, make_response
+from flask import Flask, url_for, request, session, redirect, render_template, make_response, flash
 from flask_assets import Environment, Bundle
 from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
@@ -8,12 +8,23 @@ from pprint import pprint
 import secrets
 import hashlib
 import pathlib
+import re
 import helpers
+
 
 CWD = pathlib.Path.cwd()
 STATUS_USER = 1
 STATUS_MOD = 2
 STATUS_ADMIN = 3
+DEFAULT_LENGTH = 30
+USERNAME_MIN = 2
+USERNAME_LENGTH = 20
+USERNAME_PATTERN = '[A-Za-z0-9_-]+'
+USER_PASSWORD_MIN = 2
+USER_PASSWROD_LENGTH = 200
+ANON_PASSWROD_LENGTH = 100
+RESOURCE_LENGTH = 200
+FORM_LOGIN = ['Name', 'Password']
 
 app = Flask(__name__)
 app.config.update(
@@ -60,11 +71,6 @@ posts       > many-to-many -<   resources
   posts     -- one-to-many -<   attachments
   resources -- one-to-many -<   attachments
 """
-DEFAULT_LENGTH = 30
-USERNAME_LENGTH = 20
-USER_PASSWROD_LENGTH = 200
-ANON_PASSWROD_LENGTH = 100
-RESOURCE_LENGTH = 200
 
 class Statuses(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -137,6 +143,48 @@ def index():
     return render_template("index.html"), 200
 
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    template_form = None
+
+    if request.method == 'GET':
+        template_form = FORM_LOGIN
+        code = 200
+
+    else:
+        username = request.form.get('Name')
+        user_password = request.form.get('Password')
+
+        error = False
+        if not USERNAME_MIN <= len(username) <= USERNAME_LENGTH:
+            flash("Please match the username length.")
+            error = True
+        if not re.search(USERNAME_PATTERN, username):
+            flash("Please match the username format.")
+            error = True
+        if not USER_PASSWORD_MIN <= len(user_password) <= USER_PASSWROD_LENGTH:
+            flash("Please match the password length.")
+            error = True
+
+        # TODO: db execute SELECT (login, status) FROM users WHERE login = username AND password = user_password;
+        #r = Users.query.filter_by(login=username)
+        #print(r)
+
+        if error:
+            template_form = FORM_LOGIN
+            code = 403
+        else:
+            flash("Welcome back!")
+            code = 200
+
+    return render_template(
+        "login.html", form=template_form,
+        user_min=USERNAME_MIN, user_max=USERNAME_LENGTH,
+        pass_min=USER_PASSWORD_MIN, pass_max=USER_PASSWROD_LENGTH,
+        pattern=USERNAME_PATTERN
+    ), code
+
+
 @app.route("/brotherhood/")
 @app.route("/b/")
 @app.route("/b")
@@ -156,11 +204,6 @@ def animation():
 @app.route("/u")
 def university():
     return render_template("university.html"), 200
-
-
-@app.route("/login")
-def login():
-    return render_template("login.html"), 200
 
 
 if __name__ == '__main__':
